b028338e741c5b52cb8fb7d76039ce7acc54c31f
sravya <shravyakommula1015@gmail.com>
2024-02-16 02:27:07 -0500

    created a logic for addition of numbers

--- source_code/server_side/app.js
+++ source_code/server_side/app.js
@@ -5,6 +5,18 @@ const port = 3000; //Port on which my server is running
 // Helps to parse the JSON
 app.use(express.json());
 
+// Logic for addition two numbers
+app.post('/add', (req, res) => {
+  const { numOne, numTwo } = req.body;
+
+  if (numOne == null || numTwo == null) {
+    return res.status(400).json({ error: 'Please enter valid inputs' });
+  }
+
+  const response = numOne + numTwo;
+  res.json({ sum: response });
+});
+
 // Start the server
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
