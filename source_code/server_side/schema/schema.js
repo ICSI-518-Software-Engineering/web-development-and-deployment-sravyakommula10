5bb65aa0439823d02fbca17fe4b2abe034e6d9d6
sravya <shravyakommula1015@gmail.com>
2024-04-10 22:50:48 -0400

    use bcrypt to hash passwords and store them

--- source_code/server_side/schema/schema.js
+++ source_code/server_side/schema/schema.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
 
 const inventorySchema = new mongoose.Schema({
     name: String,
@@ -6,7 +7,7 @@ const inventorySchema = new mongoose.Schema({
     image: String
 });
 
-const userSchema = new mongoose.Schema({
+const userSchema = mongoose.Schema({
     name:{
         type: String,
         required: true
@@ -23,6 +24,31 @@ const userSchema = new mongoose.Schema({
 },
 {timeStamps: true}
 );
+
+userSchema.methods.matchPassword = async function(enteredPassword){
+    return await bcrypt.compare(enteredPassword, this.password);
+}
+
+userSchema.pre('save', async function (next) {
+  const user = this;
+
+  // If password is not modified, move on to the next middleware
+  if (!user.isModified('password')) {
+    return next();
+  }
+
+  try {
+    // Generate a salt to hash the password
+    const salt = await bcrypt.genSalt(10);
+    // Hash the password using the salt
+    const hashedPassword = await bcrypt.hash(user.password, salt);
+    // Replace the plain text password with the hashed password
+    user.password = hashedPassword;
+    next();
+  } catch (error) {
+    return next(error);
+  }
+});
 
 const Inventory = mongoose.model('Inventory', inventorySchema);
 const User = mongoose.model('User', userSchema);
