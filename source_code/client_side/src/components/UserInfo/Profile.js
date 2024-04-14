8245454f3a3d084e53f25ff2d5c71e101f35a75a
sravya <shravyakommula1015@gmail.com>
2024-04-13 22:26:58 -0400

    Style changes for user information page

--- source_code/client_side/src/components/UserInfo/Profile.js
+++ source_code/client_side/src/components/UserInfo/Profile.js
@@ -1,15 +1,31 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
+import { Container, Card, Button } from 'react-bootstrap';
 
 const Profile = () => {
     const userInfo = JSON.parse(localStorage.getItem('userInfo'));
     const name = userInfo?.name;
     const email = userInfo?.email;
+    const navigate = useNavigate();
+
+    const logout = () => {
+        localStorage.removeItem('userInfo');
+        navigate('/');
+    }
 
     return (
-        <div>
-            <h2>Name: {name}</h2>
-            <h2>Email: {email}</h2>
-        </div>
+        <Container className="d-flex justify-content-center" style={{ marginTop: '50px' }}>
+            <Card style={{ width: '50rem', height: '30rem' }}>
+                <Card.Body className="d-flex flex-column justify-content-center align-items-center">
+                    <Card.Title className="text-center" style={{ fontSize: '2rem' }}>Logged-in User Information</Card.Title>
+                    <Card.Text className="d-flex flex-column align-items-center" style={{ fontSize: '1.5rem' }}>
+                        <p >Name: {name}</p>
+                        <p >Email: {email}</p>
+                    </Card.Text>
+                    <Button variant="danger" onClick={logout} style={{ fontSize: '1.25rem', padding: '10px 20px' }}>Logout</Button>
+                </Card.Body>
+            </Card>
+        </Container>
     );
 };
 
